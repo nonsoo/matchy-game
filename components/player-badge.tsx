8ac21f86c54cdc69a138f1a1ea1242bf38ee15fe
Nonso <nonsootoh@gmail.com>
2023-12-03 15:47:10 -0500

    chore: adds leader board variant

--- components/player-badge.tsx
+++ components/player-badge.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "@styles/components/player-badge.module.css";
+import { FaCrown } from "react-icons/fa6";
+import IconContext from "@utils/context/IconContext";
 
 interface DefaultVariant {
   variant: "default";
@@ -9,15 +11,39 @@ interface PointVariant {
   variant: "point";
   point: number;
 }
+
+interface LeaderBoardVariant {
+  variant: "leader_board";
+  point: number;
+  crown: boolean;
+}
 interface Props {
   name: string;
-  variant?: DefaultVariant | PointVariant;
+  variant?: DefaultVariant | PointVariant | LeaderBoardVariant;
 }
 
 const Player_badge = ({ name, variant = { variant: "default" } }: Props) => {
   if (variant.variant === "point")
     return (
       <section className={styles.mainWrapper}>
+        <div className={styles.circle}></div>
+        <span className={styles.playerName}>{name}</span>
+        <span className={styles.playerName}>Points: {variant.point}</span>
+      </section>
+    );
+
+  if (variant.variant === "leader_board")
+    return (
+      <section
+        className={styles.mainWrapper}
+        data-is_leaderboard={variant.crown}
+      >
+        {variant.crown && (
+          <IconContext className={styles.crown}>
+            <FaCrown />
+          </IconContext>
+        )}
+
         <div className={styles.circle}></div>
         <span className={styles.playerName}>{name}</span>
         <span className={styles.playerName}>Points: {variant.point}</span>
